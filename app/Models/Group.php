c375788b9149662f6e96cac6647c4c934ad4445d
Andrey Semenov <semenov.orc@gmail.com>
2025-01-18 15:03:04 +0300

    add data to Group

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    protected $fillable = ['title', 'start_from', 'is_active'];
+    protected $table = 'groups';
 }
--- database/migrations/2025_01_18_115017_create_groups_table.php
+++ database/migrations/2025_01_18_115017_create_groups_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->date('start_from');
+            $table->boolean('is_active');
         });
     }
 
